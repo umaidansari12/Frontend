11ff9813ce27a3ecd9d38069cc768db0b6012dd5
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-19 19:57:43 +0530

    update

---
+++ custom.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.carousel-slider').slick({
+        dots:false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight:true,
+        arrows:true,
+      });
+});
